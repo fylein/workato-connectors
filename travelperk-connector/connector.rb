f5e1699bfbec57a997e28c0e8be8e6442338ea87
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-09-19 23:53:38 +0530

    init: adding travelperk connector sdk (#1)
    init: adding travelperk connector sdk (#1)

---
+++ travelperk-connector/connector.rb
@@ -0,0 +1,37 @@
+{
+  title: 'Travelperk Connector',
+
+  connection: {
+    authorization: {
+      type: 'custom_auth'
+    },
+
+    base_uri: lambda do |connection|
+      ''
+    end
+  },
+
+  test: lambda do |connection|
+
+  end,
+
+  actions: {
+
+  },
+
+  triggers: {
+
+  },
+
+  methods: {
+
+  },
+
+  object_definitions: {
+
+  },
+
+  pick_lists: {
+
+  }
+}
