c01de4b6d6c33beaaf0fbc45d5eb8d58702a6cd7
Nilesh Pant <nileshpant6969@gmail.com>
2023-05-10 13:06:19 +0530

    added tests for object definations

--- fyle/spec/object_definitions/source_account_spec.rb
+++ fyle/spec/object_definitions/source_account_spec.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
-RSpec.describe 'object_definition/source_account', :vcr do
+RSpec.describe "object_definition/source_account", :vcr do
 
   # Spec describes the most commons blocks of an object definition.
   # Learn more: https://docs.workato.com/developing-connectors/sdk/cli/reference/rspec-commands.html
 
-  let(:connector) { Workato::Connector::Sdk::Connector.from_file('connector.rb', settings) }
+  let(:connector) { Workato::Connector::Sdk::Connector.from_file("connector.rb", settings) }
   let(:settings) { Workato::Connector::Sdk::Settings.from_default_file }
 
   let(:object_definition) { connector.object_definitions.source_account }
 
-  describe 'fields' do
-    subject(:schema_fields) { object_definition.fields(settings, config_fields) }
+  describe "fields" do
+    subject(:schema_fields) { object_definition.fields(settings) }
+    let(:expected_output) { JSON.parse(File.read("fixtures/object_definations/source_account.json")) }
 
-    pending 'add some examples'
+    context "give valid input" do
+      it "gives expected output" do
+        expect(schema_fields).to eq(expected_output)
+      end
+    end
   end
 end
--- fyle/spec/object_definitions/system_category_spec.rb
+++ fyle/spec/object_definitions/system_category_spec.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
-RSpec.describe 'object_definition/system_category', :vcr do
+RSpec.describe "object_definition/system_category", :vcr do
 
   # Spec describes the most commons blocks of an object definition.
   # Learn more: https://docs.workato.com/developing-connectors/sdk/cli/reference/rspec-commands.html
 
-  let(:connector) { Workato::Connector::Sdk::Connector.from_file('connector.rb', settings) }
+  let(:connector) { Workato::Connector::Sdk::Connector.from_file("connector.rb", settings) }
   let(:settings) { Workato::Connector::Sdk::Settings.from_default_file }
 
   let(:object_definition) { connector.object_definitions.system_category }
 
-  describe 'fields' do
-    subject(:schema_fields) { object_definition.fields(settings, config_fields) }
+  describe "fields" do
+    subject(:schema_fields) { object_definition.fields(settings) }
+    let(:expected_output) { JSON.parse(File.read("fixtures/object_definations/system_category.json")) }
 
-    pending 'add some examples'
+    context "give valid input" do
+      it "gives expected output" do
+        expect(schema_fields).to eq(expected_output)
+      end
+    end
   end
 end
