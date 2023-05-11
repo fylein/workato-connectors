256e51ea5b0e0a140cf50640b0a9266a35802247
Nilesh Pant <nileshpant6969@gmail.com>
2023-05-11 16:52:03 +0530

    Tests For Travelperk SDK

---
+++ travelperk-connector/spec/actions/flatten_travelperk_lines_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+RSpec.describe "actions/flatten_travelperk_lines", :vcr do
+
+  # Spec describes the most commons blocks of an action. Remove describes that you don't need.
+  # Learn more: https://docs.workato.com/developing-connectors/sdk/cli/reference/rspec-commands.html
+
+  subject(:output) { connector.actions.flatten_travelperk_lines(input) }
+
+  let(:connector) { Workato::Connector::Sdk::Connector.from_file("connector.rb", settings) }
+  let(:settings) { Workato::Connector::Sdk::Settings.from_default_file }
+  let(:input) { JSON.parse(File.read("fixtures/payload.json")) }
+
+  # Or add more fine grained tests for each action definition block
+  let(:action) { connector.actions.flatten_travelperk_lines }
+
+  describe "execute" do
+    subject(:output) { action.execute(settings, input) }
+    let(:expected_output) { JSON.parse(File.read("fixtures/actions/flatten_invoices/output.json")) }
+
+    context "give valid input" do
+      it "gives expected output" do
+        expect(output).to eq(expected_output)
+      end
+    end
+  end
+
+  describe "input_fields" do
+    subject(:input_fields) { action.input_fields(settings) }
+    let(:expected_input_fields) { JSON.parse(File.read("fixtures/actions/flatten_invoices/input_fields.json")) }
+
+    context "give valid input" do
+      it "gives expected output" do
+        expect(input_fields).to eq(expected_input_fields)
+      end
+    end
+  end
+
+  describe "output_fields" do
+    subject(:output_fields) { action.output_fields(settings) }
+    let(:expected_output_fields) { JSON.parse(File.read("fixtures/actions/flatten_invoices/output_fields.json")) }
+
+    context "give valid input" do
+      it "gives expected output" do
+        expect(output_fields).to eq(expected_output_fields)
+      end
+    end
+  end
+end
---
+++ travelperk-connector/spec/object_definitions/invoices_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.describe "object_definition/invoices", :vcr do
+
+  # Spec describes the most commons blocks of an object definition.
+  # Learn more: https://docs.workato.com/developing-connectors/sdk/cli/reference/rspec-commands.html
+
+  let(:connector) { Workato::Connector::Sdk::Connector.from_file("connector.rb") }
+  let(:object_definition) { connector.object_definitions.invoices }
+
+  describe "fields" do
+    subject(:schema_fields) { object_definition.fields }
+    let(:expected_output) { JSON.parse(File.read("fixtures/object_definations/invoices.json")) }
+
+    context "give valid input" do
+      it "gives expected output" do
+        expect(schema_fields).to eq(expected_output)
+      end
+    end
+  end
+end
